48983d5112710913c5a9485c1837bacd39934303
Husayn <husayn.mohammadi@outlook.com>
2023-09-08 18:58:28 +0330

    test: CW-42-55-10-T but with mild steel + lsr=16

--- Input/materialParameters.py
+++ Input/materialParameters.py
@@ -15,7 +15,7 @@ exec(open("Input/inputData.py").readlines()[9])     # It SHOULD read and execute
 #    Concrete Parameters:
 #=============================================================================
 
-fpc         = 44.90 *MPa                                        # Masoumeh Asgharpoor: scope of study 32.5<fpc<102 (MPa)
+fpc         = 25 *MPa                                               # Masoumeh Asgharpoor: scope of study 32.5<fpc<102 (MPa)
 print(f"fpc\t\t= {fpc/MPa:.2f} MPa")
 # Ec        = 4700*abs(fpc/MPa)**0.5s                               # With fpc in MPa  ==> ACI 318 - 2019 SI
 # Ec        = 57000*abs(fpc)**0.5                                   # With fpc in psi  ==> ACI 318 - 2019 US
@@ -41,7 +41,7 @@ nu          = 0.28
 
 ## Steel Material No. 1
 Es1         = 200   *GPa
-Fy1         = 422   *MPa
+Fy1         = 350   *MPa
 Esh1        = Es1/30
 Fu1         = 473   *MPa
 epsy1       = Fy1/Es1
@@ -60,7 +60,7 @@ eps_ult2    = eps_ult1
 alpha2      = 0.65 # Fatigue Ductility Exponent (For ASTM A572 alpha=0.65 and for ASTM A36 alpha=0.55)
 
 b           = 114 *mm  # minimum unsupported width of confined region in composite wall cross-section
-lsr         = 24. # lsr = lu/db = 12**0.5 * lu/tw
+lsr         = 16. # lsr = lu/db = 12**0.5 * lu/tw
 beta        = 1.0
 gamma       = 1.0
 Cf          = 0.5

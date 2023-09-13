e06d129ca3655c1ec53d0cc943ce5fd3d49ccaeb
Husayn <husayn.mohammadi@outlook.com>
2023-09-13 15:50:20 +0330

    refactor: added convergent algorithm (Part 1)
    In this part only tester and algorithm were added

--- functions/FuncAnalysis.py
+++ functions/FuncAnalysis.py
@@ -1,5 +1,10 @@
 import openseespy.opensees as ops
+import time
+import sys
 
+waitTime        = 1
+testerList      = ['NormDispIncr', 'EnergyIncr', 'NormUnbalance']
+algorithmList   = ['Linear', 'Newton', 'KrylovNewton', 'NewtonLineSearch', 'ModifiedNewton', 'RaphsonNewton', 'SecantNewton']
 
 def gravity(Py, ControlNode):
     
@@ -29,7 +34,7 @@ def gravity(Py, ControlNode):
     ops.analyze(1)
     # ops.loadConst('-time', 0.0)
 
-def pushoverDCF(dispTarget, ControlNode, numIncr, algorithm='KrylovNewton'): # Linear, Newton, NewtonLineSearch, ModifiedNewton, KrylovNewton, SecantNewton, RaphsonNewton, PeriodicNewton, BFGS, Broyden
+def pushoverDCF(dispTarget, ControlNode, numIncr): # Linear, Newton, NewtonLineSearch, ModifiedNewton, KrylovNewton, SecantNewton, RaphsonNewton, PeriodicNewton, BFGS, Broyden
     
     ControlNodeDoF  = 1
     dForce          = 1 # The pushover curve is not dependent to the value of dForce
@@ -57,27 +62,51 @@ def pushoverDCF(dispTarget, ControlNode, numIncr, algorithm='KrylovNewton'): # L
     ops.constraints('Transformation')
     ops.numberer('RCM')
     ops.system('BandGen')
-    ops.test('NormUnbalance', tol, numIter)
-    # ops.test('NormUnbalance', tol, numIter)
-    ops.algorithm(algorithm)
-    #   integrator('DisplacementControl', nodeTag,     dof,            incr, numIter=1, dUmin=incr, dUmax=incr)
-    ops.integrator('DisplacementControl', ControlNode, ControlNodeDoF, incr)
-    ops.analysis('Static')
     
-    # Run Analysis
-    #        analyze(numIncr=1, dt=0.0, dtMin=0.0, dtMax=0.0, Jd=0)
-    OK = ops.analyze(numIncr)
+    for tester in testerList:
+        ops.test(tester, tol, numIter)
+        for algorithm in algorithmList:
+            ops.algorithm(algorithm)  
+            #   integrator('DisplacementControl', nodeTag,     dof,            incr, numIter=1, dUmin=incr, dUmax=incr)
+            ops.integrator('DisplacementControl', ControlNode, ControlNodeDoF, incr)
+            ops.analysis('Static')
+            
+            # Run Analysis
+            #        analyze(numIncr=1, dt=0.0, dtMin=0.0, dtMax=0.0, Jd=0)
+            OK = ops.analyze(numIncr)
+            print(f"AnalyzeOutput = {OK}")
+            curD    = ops.nodeDisp(ControlNode, ControlNodeDoF)
+            print(f"\n\ndispTarget =\t{dispTarget:.4f}")
+            print(f"======>>> Current Displacement is {curD:.4f}")
+            print(f"tester:\t\t{tester}")
+            print(f"Algorithm:\t{algorithm}")
+            if OK == 0:
+                break
+            elif OK != 0:
+                print(f"\n=============== The algorithm {algorithm} failed to converge!!! ===============")
+                time.sleep(waitTime)
+                if tester == testerList[-1] and algorithm == algorithmList[-1]:
+                    print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                    print("*!*!*!*!*!* The monotonic pushover analysis failed to converge!!! *!*!*!*!*!*")
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"); sys.exit()
+            
+        if OK == 0:
+            break
+        elif OK != 0:
+            print(f"\n=============== The tester {tester} failed to converge!!! ===============")
+            time.sleep(waitTime)
+            
     
     return OK
 
 
-def cyclicAnalysis(dispTarList, ControlNode, numIncr, numCyclesPerDispTarget=2, algorithm='Newton', system='UmfPack'):
+def cyclicAnalysis(dispTarList, ControlNode, numIncr, numCyclesPerDispTarget=2):
     
     ControlNodeDoF  = 1
     dForce          = 1 # The pushover curve is not dependent to the value of dForce
     
-    tol         = 1e-8
-    numIter     = 50
+    tol         = 1e-4
+    numIter     = 200
     
     #  Define Time Series: Constant/Linear/Trigonometric/Triangular/Rectangular/Pulse/Path TimeSeries
     tagTSLinear     = 1
@@ -97,25 +126,49 @@ def cyclicAnalysis(dispTarList, ControlNode, numIncr, numCyclesPerDispTarget=2, 
     ops.wipeAnalysis()
     ops.constraints('Transformation')
     ops.numberer('RCM') # Plain, RCM, AMD, ParallelPlain, ParallelRCM
-    ops.system(system) # BandGen, BandSPD, ProfileSPD, SuperLU, UmfPack, FullGeneral, SparseSYM, ('Mumps', '-ICNTL14', icntl14=20.0, '-ICNTL7', icntl7=7)
-    ops.test('NormUnbalance', tol, numIter) # NormUnbalance, NormDispIncr, EnergyIncr, RelativeNormUnbalance, RelativeNormDispIncr, RelativeTotalNormDispIncr, RelativeEnergyIncr, FixedNumIter, NormDispAndUnbalance, NormDispOrUnbalance
-    # ops.test('NormUnbalance', tol, numIter)
-    # algorithm = 'KrylovNewton' # Linear, Newton, NewtonLineSearch, ModifiedNewton, KrylovNewton, SecantNewton, RaphsonNewton, PeriodicNewton, BFGS, Broyden
-    ops.algorithm(algorithm)  
-    print(f"Algorithm: {algorithm}")
+    ops.system('UmfPack') # BandGen, BandSPD, ProfileSPD, SuperLU, UmfPack, FullGeneral, SparseSYM, ('Mumps', '-ICNTL14', icntl14=20.0, '-ICNTL7', icntl7=7)
     # Run Analysis
     for dispTarget in dispTarList:
-        print(f"dispTarget is {dispTarget:.4f}")
         incr        = dispTarget/numIncr
         incrList                    = [incr, -incr, -incr, incr] * numCyclesPerDispTarget
         for incr in incrList:
             
-            #   integrator('DisplacementControl', nodeTag,     dof,            incr, numIter=1, dUmin=incr, dUmax=incr)
-            ops.integrator('DisplacementControl', ControlNode, ControlNodeDoF, incr)
-            ops.analysis('Static')
-            
-            #        analyze(numIncr=1, dt=0.0, dtMin=0.0, dtMax=0.0, Jd=0)
-            ops.analyze(numIncr)
+            for tester in testerList:
+                ops.test(tester, tol, numIter) # NormUnbalance, NormDispIncr, EnergyIncr, RelativeNormUnbalance, RelativeNormDispIncr, RelativeTotalNormDispIncr, RelativeEnergyIncr, FixedNumIter, NormDispAndUnbalance, NormDispOrUnbalance
+                # ops.test('NormUnbalance', tol, numIter)
+                # algorithm = 'KrylovNewton' # Linear, Newton, NewtonLineSearch, ModifiedNewton, KrylovNewton, SecantNewton, RaphsonNewton, PeriodicNewton, BFGS, Broyden
+                
+                for algorithm in algorithmList:
+                    ops.algorithm(algorithm)  
+                    #   integrator('DisplacementControl', nodeTag,     dof,            incr, numIter=1, dUmin=incr, dUmax=incr)
+                    ops.integrator('DisplacementControl', ControlNode, ControlNodeDoF, incr)
+                    ops.analysis('Static')
+                    
+                    # Run Analysis
+                    #        analyze(numIncr=1, dt=0.0, dtMin=0.0, dtMax=0.0, Jd=0)
+                    OK = ops.analyze(numIncr)
+                    print(f"AnalyzeOutput = {OK}")
+                    curD    = ops.nodeDisp(ControlNode, ControlNodeDoF)
+                    print(f"\n\ndispTarget =\t{dispTarget:.4f}")
+                    print(f"======>>> Current Displacement is {curD:.4f}")
+                    print(f"tester:\t\t{tester}")
+                    print(f"Algorithm:\t{algorithm}")
+                    if OK == 0:
+                        break
+                    elif OK != 0:
+                        print(f"\n=============== The algorithm {algorithm} failed to converge!!! ===============")
+                        time.sleep(waitTime)
+                        if tester == testerList[-1] and algorithm == algorithmList[-1]:
+                            print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                            print("*!*!*!*!*!* The cyclic pushover analysis failed to converge!!! *!*!*!*!*!*!*!*")
+                            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"); sys.exit()
+                    
+                if OK == 0:
+                    break
+                elif OK != 0:
+                    print(f"\n=============== The tester {tester} failed to converge!!! ===============")
+                    time.sleep(waitTime)
+                
 
 
 

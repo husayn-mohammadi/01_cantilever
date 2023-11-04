602f85461965bfc80f4f86e81786f82de65f57d1
Husayn <husayn.mohammadi@outlook.com>
2023-11-04 03:50:17 +0330

    refactor: uniaxialMaterial definer added

--- functions/ClassComposite.py
+++ functions/ClassComposite.py
@@ -1,16 +1,17 @@
-exec(open("MAIN.py").readlines()[18]) # It SHOULD read and execute exec(open("Input/units    .py").read())
-# exec(open("../Input/unitsSI.py").read()) # It SHOULD read and execute exec(open("Input/units    .py").read())
+# exec(open("MAIN.py").readlines()[18]) # It SHOULD read and execute exec(open("Input/units    .py").read())
+exec(open("../Input/unitsSI.py").read()) # It SHOULD read and execute exec(open("Input/units    .py").read())
 import sys
-import numpy as np
+import numpy                       as np
+import openseespy.opensees         as ops
 
 class steel:
     def __init__(self, Es, Fy, Fu, eps_sh, eps_ult, nu, 
                  alpha, beta, gamma, Cf, a1, limit, R1, R2, R3):
         self.Es     = Es
-        self.Esh    = self.Es/30
+        self.Esh    = Es/30
         self.Fy     = Fy 
         self.Fu     = Fu 
-        self.epsy   = self.Fy/self.Es
+        self.epsy   = Fy/Es
         self.eps_sh = eps_sh
         self.eps_ult= eps_ult
         self.nu     = nu
@@ -30,57 +31,56 @@ class concrete:
         self.fpcu       = 1
         self.wc         = wc
         self.lam        = lam
-        self.Ec         = (21.5e3*MPa * 1.0 * (abs(self.fpc/MPa)/10)**(1/3))            # Tangent Modulus of Elasticity with fpc in MPa  ==> CEB-FIB-2010 5.1.7.2 (Selected by Masoumeh Asgharpoor)
-        self.epsc0      = 2*self.fpc/self.Ec                                            # Crack width (Average of 0.15 to  0.25 mm)
-        self.Gf         = (73 * (self.fpc/MPa)**0.18)                                   # Fracture Energy CEB-FIB-2010 section 5.1.5.2
-        self.fts        = 1.0*(0.3 * (self.fpc/MPa - 8)**(2/3))*MPa                     # CEB-FIB-2010 Eq. (5.1-5)
-        self.Ets        = abs(1/(1-(2*self.Ec*self.Gf)/(self.wc*self.fts**2)))*self.Ec
+        self.Ec         = (21.5e3*MPa * 1.0 * (abs(fpc/MPa)/10)**(1/3))            # Tangent Modulus of Elasticity with fpc in MPa  ==> CEB-FIB-2010 5.1.7.2 (Selected by Masoumeh Asgharpoor)
+        self.epsc0      = 2*fpc/self.Ec                                            # Crack width (Average of 0.15 to  0.25 mm)
+        self.epscU      = 1
+        self.Gf         = (73 * (fpc/MPa)**0.18)                                   # Fracture Energy CEB-FIB-2010 section 5.1.5.2
+        self.fts        = 1.0*(0.3 * (fpc/MPa - 8)**(2/3))*MPa                     # CEB-FIB-2010 Eq. (5.1-5)
+        self.Ets        = abs(1/(1-(2*self.Ec*self.Gf)/(wc*self.fts**2)))*self.Ec
         
 
 class compo:
-    def __init__(self, tagSec, tagInt, ALR,
-                 tw, Hw, Esw, Fyw, Fuw, eps_shw, eps_ultw, nuw, alphaw, betaw, gammaw, Cfw, a1w, limitw, R1w, R2w, R3w, 
-                 Bf, tf, Esf, Fyf, Fuf, eps_shf, eps_ultf, nuf, alphaf, betaf, gammaf, Cff, a1f, limitf, R1f, R2f, R3f,
-                 tc, fpc, wc, lamConf, lamUnconf,
-                 lsr, b):
+    def __init__(self, tagSec, tagMatStFlange, tagMatStWeb, tagMatCtUnconf, tagMatCtConf, ALR, lsr, b,
+                 tw, Hw, Esw, Fyw, Fuw, eps_shw, eps_ultw, nuw, alphaw, betaw, gammaw, Cfw, a1w, limitw, 
+                 Bf, tf, Esf, Fyf, Fuf, eps_shf, eps_ultf, nuf, alphaf, betaf, gammaf, Cff, a1f, limitf,
+                 tc, fpc, wc, lamConf, lamUnconf
+                 ):
         
         self.tagSec     = tagSec
-        self.tagInt     = tagInt
         self.ALR        = ALR
         self.lsr        = lsr
         self.b          = b
         self.St_flange  = steel(Esw, Fyw, Fuw, eps_shw, eps_ultw, nuw, alphaw, betaw, gammaw, Cfw, a1w, limitw, R1w, R2w, R3w)
         self.St_web     = steel(Esf, Fyf, Fuf, eps_shf, eps_ultf, nuf, alphaf, betaf, gammaf, Cff, a1f, limitf, R1f, R2f, R3f)
         self.Ct_unconf  = concrete(fpc,  wc, lamUnconf)
-        self.Es         = (self.St_flange.Es        + self.St_web.Es        )/2
-        self.Fy         = (self.St_flange.Fy        + self.St_web.Fy        )/2
-        self.Fu         = (self.St_flange.Fu        + self.St_web.Fu        )/2
-        self.eps_sh     = (self.St_flange.eps_sh    + self.St_web.eps_sh    )/2
-        self.eps_ult    = (self.St_flange.eps_ult   + self.St_web.eps_ult   )/2
-        self.nu         = (self.St_flange.nu        + self.St_web.nu        )/2
-        self.alpha      = (self.St_flange.alpha     + self.St_web.alpha     )/2
-        self.beta       = (self.St_flange.beta      + self.St_web.beta      )/2
-        self.gamma      = (self.St_flange.gamma     + self.St_web.gamma     )/2
-        self.Cf         = (self.St_flange.Cf        + self.St_web.Cf        )/2
-        self.a1         = (self.St_flange.a1        + self.St_web.a1        )/2
-        self.limit      = (self.St_flange.limit     + self.St_web.limit     )/2
-        self.R1         = (self.St_flange.R1        + self.St_web.R1        )/2
-        self.R2         = (self.St_flange.R2        + self.St_web.R2        )/2
-        self.R3         = (self.St_flange.R3        + self.St_web.R3        )/2
+        self.Es         = Es        = (self.St_flange.Es        + self.St_web.Es        )/2
+        self.Esh        = Esh       = (self.St_flange.Esh       + self.St_web.Esh       )/2
+        self.Fy         = Fy        = (self.St_flange.Fy        + self.St_web.Fy        )/2
+        self.Fu         = Fu        = (self.St_flange.Fu        + self.St_web.Fu        )/2
+        self.eps_sh     = eps_sh    = (self.St_flange.eps_sh    + self.St_web.eps_sh    )/2
+        self.eps_ult    = eps_ult   = (self.St_flange.eps_ult   + self.St_web.eps_ult   )/2
+        self.nu         = nu        = (self.St_flange.nu        + self.St_web.nu        )/2
+        self.alpha      = alpha     = (self.St_flange.alpha     + self.St_web.alpha     )/2
+        self.beta       = beta      = (self.St_flange.beta      + self.St_web.beta      )/2
+        self.gamma      = gamma     = (self.St_flange.gamma     + self.St_web.gamma     )/2
+        self.Cf         = Cf        = (self.St_flange.Cf        + self.St_web.Cf        )/2
+        self.a1         = a1        = (self.St_flange.a1        + self.St_web.a1        )/2
+        self.limit      = limit     = (self.St_flange.limit     + self.St_web.limit     )/2
         
-        self.rhos           = (2*tw)/(tc + 2*tw) # Percentage of Steel
-        self.R              = b/tw * (12*(1-self.nu**2)/(4*np.pi**2))**0.5 * (self.Fy/self.Es)**0.5 # Masoumeh Asgarpoor
-        self.frp            = (-6.5*self.R*((self.Ct_unconf.fpc/MPa)**1.46/(self.Fy/MPa)) + 0.12*(self.Ct_unconf.fpc/MPa)**1.03)*MPa # Masoumeh Asgarpoor
-        fpcc                = ((self.Ct_unconf.fpc/MPa) + 4*(self.frp/MPa)*(1+0.8*ALR)**3.5)*MPa # Masoumeh Asgarpoor
-        self.Ct_conf        = concrete(fpcc, wc, lamConf)
-        self.Ct_conf.Ec     = (21.5e3*MPa * 1.0 * (abs(self.Ct_conf.fpc/MPa)/10)**(1/3)) # Tangent Modulus of Elasticity with fpc in MPa  ==> CEB-FIB-2010 5.1.7.2 (Selected by Masoumeh Asgharpoor)
-        self.Ct_conf.epsc0  = 2*self.Ct_conf.fpc/self.Ct_conf.Ec
-        self.Ct_conf.fpcu   = (0.15 *(self.Ct_unconf.fpc/MPa)** 1.5 * (self.Fy/MPa)**0.01 * self.rhos**0.16 * (1-ALR)**(-0.5) * lsr**(-0.025))*MPa # Masoumeh Asgarpoor 
-        self.Ct_conf.epscU  = 0.157   *self.Ct_unconf.epsc0 **(-0.67) * (self.Ct_unconf.fpcu/MPa) **0.23 *(self.Fy/MPa)**(-1.4) * self.rhos**0.06 * (1-ALR)**(-0.17) * lsr**0.12 #Masoumeh Asgarpoor
-        self.Ct_conf.fpcu   = (2.63 *(self.Ct_conf.fpc/MPa)**0.65 * (self.Fy/MPa)**0.001 * self.rhos**(-0.04) * (1-ALR)**( 0.4) * lsr**( 0.070))*MPa # Masoumeh Asgarpoor 
-        self.Ct_conf.epscU  = 1.12e-6 *self.Ct_conf.epsc0**(-0.33) * (self.Ct_conf.fpcu/MPa)**0.83 *(self.Fy/MPa)**(0.77) * self.rhos**0.07 * (1-ALR)**( 0.15) * lsr**0.16 #Masoumeh Asgarpoor 
-        self.r              = (15*lsr**0.37 * (self.Fy/self.Ct_unconf.fpc)**0.26 * self.eps_sh**0.3 * self.eps_ult**0.85)**-1 # Masoumeh Asgarpoor 
-        self.Cd             = 16 * (self.Fy/self.Fu)**0.78 * self.eps_ult * ((self.Ct_unconf.fpcu/MPa)**0.5/(self.Ct_unconf.fpc/MPa))**0.58 * self.r**0.37 # Masoumeh Asgarpoor 
+        epsc0, fpcu         =           self.Ct_unconf.epsc0, self.Ct_unconf.fpcu
+        rhos                =           (2*tw)/(tc + 2*tw) # Percentage of Steel
+        R                   =           b/tw * (12*(1-nu**2)/(4*np.pi**2))**0.5 * (Fy/Es)**0.5 # Masoumeh Asgarpoor
+        frp                 =           (-6.5*R*((fpc/MPa)**1.46/(Fy/MPa)) + 0.12*(fpc/MPa)**1.03)*MPa # Masoumeh Asgarpoor
+        fpcc                =           ((fpc/MPa) + 4*(frp/MPa)*(1+0.8*ALR)**3.5)*MPa # Masoumeh Asgarpoor
+        self.Ct_conf        =           concrete(fpcc, wc, lamConf)
+        self.Ct_conf.Ec     = Ecc =     (21.5e3*MPa * 1.0 * (abs(fpcc/MPa)/10)**(1/3)) # Tangent Modulus of Elasticity with fpc in MPa  ==> CEB-FIB-2010 5.1.7.2 (Selected by Masoumeh Asgharpoor)
+        self.Ct_conf.epsc0  = epscc0 =  2*fpcc/Ecc
+        self.Ct_unconf.fpcu = fpcu =    (0.15 *(fpc/MPa)** 1.5 * (Fy/MPa)**0.01 * rhos**0.16 * (1-ALR)**(-0.5) * lsr**(-0.025))*MPa # Masoumeh Asgarpoor 
+        self.Ct_unconf.epscU= epscU =   0.157   *epsc0 **(-0.67) * (fpcu/MPa) **0.23 *(Fy/MPa)**(-1.4) * rhos**0.06 * (1-ALR)**(-0.17) * lsr**0.12 #Masoumeh Asgarpoor
+        self.Ct_conf.fpcu   = fpccu =   (2.63 *(fpcc/MPa)**0.65 * (Fy/MPa)**0.001 * rhos**(-0.04) * (1-ALR)**(0.4) * lsr**(0.070))*MPa # Masoumeh Asgarpoor 
+        self.Ct_conf.epscU  = epsccU =  1.12e-6 *epscc0**(-0.33) * (fpccu/MPa)**0.83 *(Fy/MPa)**(0.77) * rhos**0.07 * (1-ALR)**( 0.15) * lsr**0.16 #Masoumeh Asgarpoor 
+        self.r              = r =       (15*lsr**0.37 * (Fy/fpc)**0.26 * eps_sh**0.3 * eps_ult**0.85)**-1 # Masoumeh Asgarpoor 
+        self.Cd             = Cd =      16 * (Fy/Fu)**0.78 * eps_ult * ((fpcu/MPa)**0.5/(fpc/MPa))**0.58 * r**0.37 # Masoumeh Asgarpoor 
         
         
         
@@ -116,22 +116,45 @@ class compo:
         self.EAeff_Ct       = self.EAeff_unconf + self.EAeff_conf
         
         self.EAeff          = self.EAeff_St + self.EAeff_Ct
+        
+        self.tagMatStFlange = tagMatStFlange
+        ops.uniaxialMaterial('ReinforcingSteel', tagMatStFlange, Fyf, Fuf, Esf, Esh, eps_shf, eps_ultf, 
+                             '-GABuck', lsr, beta, r, gamma, 
+                             '-CMFatigue', Cf, alpha, Cd, 
+                             '-IsoHard', a1, limit
+                             )
+        self.tagMatStWeb    = tagMatStWeb
+        ops.uniaxialMaterial('ReinforcingSteel', tagMatStWeb, Fyw, Fuw, Esw, Esh, eps_shw, eps_ultw, 
+                             '-GABuck', lsr, beta, r, gamma, 
+                             '-CMFatigue', Cf, alpha, Cd, 
+                             '-IsoHard', a1, limit
+                             )
+        ops.uniaxialMaterial('Concrete02', tagMatCtUnconf, fpc,  epsc0,  fpcu,  self.Ct_unconf.epscU, lamUnconf, self.Ct_unconf.fts, self.Ct_unconf.Ets)
+        ops.uniaxialMaterial('Concrete02', tagMatCtConf,   fpcc, epscc0, fpccu, self.Ct_conf.epscU,   lamConf,   self.Ct_conf.fts,   self.Ct_conf.Ets)
+        
+        
+        
+        
+        
+#tags       = [tagSec, tagMatStFlange, tagMatStWeb, tagMatCtUnconf, tagMatCtConf]
+tags        = [1,      1,              2,           3,              4           ]
+#propStPart = [B,         H,         Es,      Fy,      Fu,      eps_sh, eps_ult, nu,   alpha, beta, gamma, Cf,  a1,  limit] 
+propWeb     = [3/16*inch, 0.24 *m,   200*GPa, 422*MPa, 473*MPa, 0.007,  0.12,    0.28, 0.65,  1.0,  1.0,   0.5, 4.3, 0.01]
+propFlange  = [11*inch,   3/16*inch, 200*GPa, 422*MPa, 473*MPa, 0.007,  0.12,    0.28, 0.65,  1.0,  1.0,   0.5, 4.3, 0.01]
+#propCore   = [tc,     fpc,    wc,     lamConf, lamUnconf]
+propCore    = [9*inch, 50*MPa, 0.2*mm, 0.05,    0.25     ]
+lsr         = 48.
+b           = 114*mm
+#beam= compo(*tags, ALR, lsr, b, *propWeb, *propFlange, *propCore)
+beam = compo(*tags, 0.1, lsr, b, *propWeb, *propFlange, *propCore)
 
-# #propStPart = [B,         H,         Es,      Fy,      Fu,      eps_sh, eps_ult, nu,   alpha, beta, gamma, Cf,  a1,  limit, R1,    R2,   R3 ] 
-# propWeb     = [3/16*inch, 0.24 *m,   200*GPa, 422*MPa, 473*MPa, 0.007,  0.12,    0.28, 0.65,  1.0,  1.0,   0.5, 4.3, 0.01,  0.333, 18.0, 4.0]
-# propFlange  = [11*inch,   3/16*inch, 200*GPa, 422*MPa, 473*MPa, 0.007,  0.12,    0.28, 0.65,  1.0,  1.0,   0.5, 4.3, 0.01,  0.333, 18.0, 4.0]
-# #profCore   = [tc,     fpc,    wc,     lamConf, lamUnconf]
-# profCore    = [9*inch, 50*MPa, 0.2*mm, 0.05,    0.25     ]
-# lsr         = 48.
-# b           = 114*mm
-# beam = compo(1, 1, 0.1,*propWeb, *propFlange, *profCore, lsr, b)
+print(f"Es of flange material is {beam.St_flange.Es}")
+print(f"Es of web    material is {beam.St_web.Es}")
+print(f"Average Es the beam   is {beam.Es}")
+print(f"Depth of the beam is {beam.d}")
+print(f"EIeff = {beam.EIeff}")
+print(f"EAeff = {beam.EAeff}")
 
-# print(f"Es of flange material is {beam.St_flange.Es}")
-# print(f"Es of web    material is {beam.St_web.Es}")
-# print(f"Average Es the beam   is {beam.Es}")
-# print(f"Depth of the beam is {beam.d}")
-# print(f"EIeff = {beam.EIeff}")
-# print(f"EAeff = {beam.EAeff}")
 
 
 
